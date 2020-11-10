38103630a3d19e0fb31bfbef6631059d730cf168
Dikshha2601 <66013703+Dikshha2601@users.noreply.github.com>
2020-11-10 15:30:22 +0530

    Update main.js

--- main.js
+++ main.js
@@ -26,39 +26,39 @@ function setup() {
   video = createCapture(VIDEO);
   video.hide();
 
-  posenet = ml5.poseNet(video, function () {
-    console.log("Model loaded.");
-  });
-  posenet.on("pose", function (results) {
-    if (results.length > 0) {
-      console.log(results);
+//   posenet = ml5.poseNet(video, function () {
+//     console.log("Model loaded.");
+//   });
+//   posenet.on("pose", function (results) {
+//     if (results.length > 0) {
+//       console.log(results);
 
-      leftWristX = results[0].pose.leftWrist.x;
-      leftWristY = results[0].pose.leftWrist.y;
-      console.log("Left wrist x: " + leftWristX + "\nand y: " + leftWristY);
+//       leftWristX = results[0].pose.leftWrist.x;
+//       leftWristY = results[0].pose.leftWrist.y;
+//       console.log("Left wrist x: " + leftWristX + "\nand y: " + leftWristY);
 
-      rightWristX = results[0].pose.rightWrist.x;
-      rightWristY = results[0].pose.rightWrist.y;
-      console.log("Right wrist x: " + rightWristX + "\nand y: " + rightWristY);
-    }
-  });
-}
+//       rightWristX = results[0].pose.rightWrist.x;
+//       rightWristY = results[0].pose.rightWrist.y;
+//       console.log("Right wrist x: " + rightWristX + "\nand y: " + rightWristY);
+//     }
+//   });
+// }
 
-function draw() {
-  image(video, 0, 0, 400, 300);
-  if (scoreLeftWrist > 0.2) {
-    circle(leftWristX - 10, leftWristY - 10, 20);
+// function draw() {
+//   image(video, 0, 0, 400, 300);
+//   if (scoreLeftWrist > 0.2) {
+//     circle(leftWristX - 10, leftWristY - 10, 20);
 
-    document.getElementById("result").innerHTML =
-      "Song name is 'Can't stop the feeling!' ";
-    song_final = cstfl;
-  }
-}
+//     document.getElementById("result").innerHTML =
+//       "Song name is 'Can't stop the feeling!' ";
+//     song_final = cstfl;
+//   }
+// }
 
-function play() {
-  song_final.play();
-}
+// function play() {
+//   song_final.play();
+// }
 
-function stop() {
-  song_final.stop();
-}
+// function stop() {
+//   song_final.stop();
+// }
